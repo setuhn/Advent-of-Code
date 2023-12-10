d2936df2edb41f2e77c8639f1bd350772a253f31
Setuhn <setuhn@gmail.com>
2023-12-10 22:35:50 +0100

    AoC day 4

--- 2023/day_3.py
+++ 2023/day_3.py
@@ -1,18 +1,17 @@
 import re
 
 if __name__ == '__main__':  
-    with open(f'day_3.1-test.txt') as data:
-        schematic = [line.strip() for line in data.readlines()]
+    with open(f'day_3.txt') as data:
+        schematic: list = [line.strip() for line in data.readlines()]
 
-    len_col = len(schematic)
-    len_line = len(schematic[0])
+    len_col: int = len(schematic)
+    len_line: int  = len(schematic[0])
 
-    symbol_list: list = []
-    part_dict: dict = {}
+    symbol_star_dict: dict = {}
 
-    regex_part = re.compile(r'(\d+)')
+    regex_part: re.Pattern = re.compile(r'(\d+)')
 
-    sum = 0
+    part_sum: int  = 0
 
     for l, line in enumerate(schematic):
 
@@ -21,23 +20,23 @@ if __name__ == '__main__':
             i = part.start()
             f = part.end()
 
-            # all coordinate around the part
+            # Get all the coordinates around the part
             adjacent = [(l-1, y) for y in range(i-1, f+1)] + [(l+1, y) for y in range(i-1, f+1)] + [(l, i-1)] + [(l, f)]
             adjacent = [(x, y) for x, y in adjacent if 0 <= x < len_line and 0 <= y < len_col]
 
-            # If any symbol next to it, add its value to sum and exit for loop
+            # If any symbol next to it, add its value to sum then check if there are any stars around and if yes, adds the part's value to the symbol_star_dict
             for x, y in adjacent:
                 cell = schematic[x][y]
+                
                 if not cell.isdigit() and cell != '.':
-                    sum += int(part.group())
-                    break
+                    part_sum += int(part.group())
 
-    print(f'Answer part 1: {sum}')
+                    if cell == '*':
+                        if (x, y) not in symbol_star_dict.keys():
+                            symbol_star_dict[(x, y)] = []
 
-                # part_dict[(l, i)] = part.group()
-
-        # # Find the coordinate (line, col) of every symbol -> search the adjacent cells and add the value to sum if a part is found
-        # for symbol in regex_symbol.finditer(line):
-        #     symbol_list.append((l, symbol.start()))
+                        symbol_star_dict[(x, y)].append(int(part.group()))
 
 
+    print(f'Answer part 1: {part_sum}')
+    print(f'Answer part 2: {sum([star[0]*star[1] for star in symbol_star_dict.values() if len(star) == 2])}') # Calculates the sum of all of the gear ratios
---
+++ 2023/day_4.py
@@ -0,0 +1,34 @@
+import re
+
+def increment_cards_inventory(cards_inventory, card_idx, add):
+    if card_idx not in cards_inventory.keys():
+                cards_inventory[card_idx] = 0
+
+    cards_inventory[card_idx] += add
+
+if __name__ == '__main__':
+
+    with open(f'day_4.txt') as data:
+
+        regex_numbers: re.Pattern = re.compile(r'(\d+)')
+        points = 0
+
+        cards_inventory = {}
+
+        for card in data.readlines():
+            card_idx, numbers = card.split(':')
+            card_idx = int(regex_numbers.search(card_idx).group())
+
+            increment_cards_inventory(cards_inventory, card_idx, 1)
+
+            numbers = [set(regex_numbers.findall(n)) for n in numbers.split('|')]
+            intersection = numbers[0].intersection(numbers[1])
+
+            if intersection:
+                points += 2**(len(intersection)-1)
+
+                for n in range(1, len(intersection)+1):
+                    increment_cards_inventory(cards_inventory, card_idx + n, cards_inventory[card_idx])
+
+    print(f'Answer to part 1: {points}')
+    print(f'Answer to part 2: {sum(cards_inventory.values())}')

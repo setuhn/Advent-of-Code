5cc4bb9613ef82f4b51988c063f6559e81204845
Setuhn <setuhn@gmail.com>
2023-12-11 17:32:28 +0100

    AoC day 6 - done

--- 2023/day_5.py
+++ 2023/day_5.py
@@ -20,6 +20,24 @@ def find_lowest_location(seeds_list):
                     
     return min(history['location'])
 
+def find_lowest_location_2(seeds_ranges):
+
+    for c in chunks[1:]:
+        
+        name_source, name_destination = regex_map.search(c).groups()
+
+        for line in c.split('\n')[1:]:
+            start_destination, start_source, span = [int(x) for x in line.split(' ')]
+
+            for idx, (seeds_range_start, seeds_range_end) in enumerate(seeds_ranges):
+                if start_source <= seeds_range_start < start_source + span:
+                    print(seeds_range_start - (start_source + span))
+                
+                elif start_source <= seeds_range_end < start_source + span:
+                    print(start_source - seeds_range_end)
+                    
+                    
+
 if __name__ == '__main__':
 
     with open(f'day_5.txt') as data:
@@ -32,11 +50,11 @@ if __name__ == '__main__':
 
         print(f'Answer to part 1: {find_lowest_location(seeds_planted)}')
 
-        # Find out if range of seeds to be planted in overlapping the source_range. If yes, swap only the overlapping range 
+        # Find if range of seeds to be planted is overlapping the source_range. If yes, swap only the overlapping range -> data struct using only ranges?
+        seeds_ranges = [(seeds_planted[idx], seeds_planted[idx]+seeds_planted[idx+1]) for idx in range(0, len(seeds_planted), 2)]
+        print(seeds_ranges)
 
-
-
-        
+        find_lowest_location_2(seeds_ranges)
 
 
 
---
+++ 2023/day_6.py
@@ -0,0 +1,36 @@
+import re
+from math import sqrt, ceil, floor
+
+def calculate_ways(time, distance):
+    # This problem can be simplified by solveing a second degree polynomial: -t_waited**2 + time * t_waited - distance = 0
+    # a = -1, b = time, c = -distance
+    ways_total = 1
+    for t, d in zip(time, distance):
+        delta = t**2 - 4 * d # b**2 - 4 * a * c
+        x1 = (t + sqrt(delta)) / 2 # (-b + sqrt(delta))/ (2 * a) as a = -1 -> (b + sqrt(delta)) / 2
+        x2 = (t - sqrt(delta)) / 2 # (b - sqrt(delta)) / 2
+
+        if x1.is_integer():
+            x1 -= 1
+        if x2.is_integer():
+            x2 +=1
+        ways_total *= floor(x1) - ceil(x2) + 1
+
+    return ways_total
+
+if __name__ == '__main__':
+
+    with open(f'day_6.txt') as data:
+        # table = [line.split(': ')[1].strip().split(' ') for line in data.read().strip().split('\n')]
+        lines_list = data.readlines()
+        time_1, distance_1 = [[int(n) for n in re.findall(r'(\d+)', line.strip())] for line in lines_list]
+        time_2, distance_2 = [int(''.join(re.findall(r'(\d+)', line.strip()))) for line in lines_list]
+
+        print(f'Answer to part 1: {calculate_ways(time_1, distance_1)}')
+        print(f'Answer to part 1: {calculate_ways([time_2], [distance_2])}')
+
+
+
+
+
+

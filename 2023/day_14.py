914d43d9705c28ebe57b70921e376d3a396ae469
Setuhn <setuhn@gmail.com>
2023-12-14 09:06:18 +0100

    AoC day 14 part 1

--- 2023/day_14.py
+++ 2023/day_14.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 ROCKS = {
-    'O': True,
-    '#': False,
-    '.': None
+    'O': 0,
+    '#': 2,
+    '.': 1
 }
 
 def invert_coordinates(coordinates_list):
@@ -17,16 +17,32 @@ def invert_coordinates(coordinates_list):
 if __name__ == '__main__':
     plateform = []
 
-    with open(f'day_14.1-test.txt') as data:
+    with open(f'day_14.txt') as data:
         for line in data.readlines():
             plateform.append([ROCKS[char] for char in line.strip()])
 
     plateform_array = np.array(plateform)
 
-    coordinate_round = invert_coordinates(np.argwhere(plateform_array == True).tolist())
-    coordinate_square = invert_coordinates(np.argwhere(plateform_array == False).tolist())
-
     # Find out where the square rocks are
     # Divide the columns at their idx 
-    # Find the number of round boulder in each chunk of column 
-    # Clump the round boulder together towards the north
+    # Order the chunks
+    for idx_col in range(plateform_array.shape[1]):
+        delimiters = [d[0] for d in np.argwhere(plateform_array[:, idx_col] == 2)]
+
+        if delimiters:
+            plateform_array[:delimiters[0], idx_col] = np.sort(plateform_array[:delimiters[0], idx_col])
+        
+            for idx_del, delimiter in enumerate(delimiters):
+                if delimiter != 0:
+                    plateform_array[delimiters[idx_del - 1]+1:delimiter, idx_col] = np.sort(plateform_array[delimiters[idx_del - 1]+1:delimiter, idx_col])
+            
+            plateform_array[delimiters[-1] + 1:, idx_col] = np.sort(plateform_array[delimiters[-1] + 1:, idx_col])
+        
+        else:
+            plateform_array[:, idx_col] = np.sort(plateform_array[:, idx_col])
+
+    load = 0
+    for idx_row in range(plateform_array.shape[1]):
+        load += len(np.argwhere(plateform_array[idx_row, :] ==0)) * (plateform_array.shape[1] - idx_row)
+
+    print(f'Answer to part 1: {load}')

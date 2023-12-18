3966ea8a13f03f357cade9b4ceb07a958039ab25
Setuhn <setuhn@gmail.com>
2023-12-18 15:19:46 +0100

    Advances day 16

--- 2023/day_16.py
+++ 2023/day_16.py
@@ -14,16 +14,17 @@ def get_next_nodes(node, layout_array):
     next_nodes = {}
     next_nodes_list = {}
     idx_line, idx_col = node.coordinates
+    boundary_line, boundary_col = layout_array.shape
 
     next_nodes_list['down'] = layout_array[idx_line+1:, idx_col][layout_array[idx_line+1:, idx_col] != None] #-> [0]
     next_nodes_list['up'] = layout_array[:idx_line, idx_col][layout_array[:idx_line, idx_col] != None] #-> [-1]
     next_nodes_list['right'] = layout_array[idx_line, idx_col+1:][layout_array[idx_line, idx_col+1:] != None] # -> [0]
     next_nodes_list['left'] = layout_array[idx_line, :idx_col][layout_array[idx_line, :idx_col] != None] # -> [-1]
     
-    down_node = next_nodes_list['down'][0] if any(next_nodes_list['down']) else None
-    up_node = next_nodes_list['up'][-1] if any(next_nodes_list['up']) else None
-    right_node = next_nodes_list['right'][0] if any(next_nodes_list['right']) else None
-    left_node = next_nodes_list['left'][-1] if any(next_nodes_list['left']) else None
+    down_node = next_nodes_list['down'][0] if any(next_nodes_list['down']) else (boundary_line-1, idx_col)
+    up_node = next_nodes_list['up'][-1] if any(next_nodes_list['up']) else (0, idx_col)
+    right_node = next_nodes_list['right'][0] if any(next_nodes_list['right']) else (idx_line, boundary_col-1)
+    left_node = next_nodes_list['left'][-1] if any(next_nodes_list['left']) else (idx_line, 0)
 
     if node.char == '|':
         
@@ -104,16 +105,58 @@ if __name__ == '__main__':
             layout.append([Node(char, (idx_line, idx_col)) if char != '.' else None for idx_col, char in enumerate(line.strip())])
 
     layout_array = np.array(layout)
-    coordinates_mirrors_splitters = np.argwhere(layout_array)
+    coordinates_mirrors_splitters = np.argwhere(layout_array != None)
 
 
     for coordinates in coordinates_mirrors_splitters.tolist():
         node = layout_array[coordinates[0],  coordinates[1]]
         next_nodes = get_next_nodes(node, layout_array)
-        node.next = next_nodes
-        for previous_node, next_node in next_nodes.items():
-            if not isinstance(previous_node, Node):
-                starting_node = node
+        node.next_node_from = next_nodes
     
+    first_node = layout_array[coordinates_mirrors_splitters[0][0], coordinates_mirrors_splitters[0][1]]
+    print()
+    print(first_node.next_node_from)
+    print()
     energised_tiles = set()
-    beams = deque(starting_node)
+    beams_list = deque()
+    beams_list.append(['start', first_node])
+    history = [['start', first_node]]
+
+    while beams_list:
+        previous_node, current_node = beams_list.popleft()
+
+        # print(f'List of beams {beams_list}')
+
+        while current_node != None:
+
+            next_node, *other_nodes = current_node.next_node_from[previous_node]
+
+            print(f'{current_node} leads to {next_node} and {other_nodes} from {previous_node}')
+
+            if other_nodes:
+
+                for node in other_nodes:
+
+                    if [current_node, node] not in history:
+
+                        beams_list.append([current_node, node])
+                        history.append([current_node, node])
+                                      
+            if next_node:
+
+                print(f'Next coordinates: {next_node.coordinates}')
+
+                if [current_node.coordinates, next_node.coordinates] in history:
+                    break
+                
+                else:
+
+                    history.append([current_node, next_node])
+                    previous_node, current_node = current_node, next_node
+
+            else:
+                break
+
+    print(len(history))
+
+            

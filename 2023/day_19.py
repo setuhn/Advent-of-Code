43af6a86e11b78f55f98e723cf2799b4c847002c
Setuhn <setuhn@gmail.com>
2023-12-23 22:33:33 +0100

    AoC advances day 19 part 2

--- 2023/day_19.py
+++ 2023/day_19.py
@@ -3,15 +3,14 @@ import timeit
 start = timeit.default_timer()
 
 class Workflow():
-    def __init__(self, name, refuse, logic, root = None) -> None:
+    def __init__(self, name, logic, root = None) -> None:
         self.name = name
-        self.refuse = refuse
         self.logic = logic
         self.root = root
 
     def evaluate(self, part):
-        for evaluation in self.logic:
-            out = self.refuse
+        for evaluation in self.logic[:-1]:
+            out = self.logic[-1]
 
             if (evaluation[1] == '<' and part[evaluation[0]] < evaluation[2]) or (evaluation[1] == '>' and part[evaluation[0]] > evaluation[2]):
                 out =  evaluation[3]
@@ -22,14 +21,22 @@ class Workflow():
 def calculate_sum_rating_scores(accepted_parts):
     return sum([sum([rating for rating in part.values()]) for part in accepted_parts])
 
-def update_ranges(ratings_range, evaluation):
+def update_ranges(ratings_range, evaluation, sense = True):
     rating, comparison, num, _ = evaluation
 
-    if comparison == '<':
-        ratings_range[rating].intersection_update(range(1, num))
+    if sense:
+        if comparison == '<':
+            ratings_range[rating].intersection_update(range(1, num))
 
-    elif comparison == '>':
-        ratings_range[rating].intersection_update(range(num + 1, 4001))
+        elif comparison == '>':
+            ratings_range[rating].intersection_update(range(num + 1, 4001))
+    
+    else:
+        if comparison == '<':
+            ratings_range[rating].intersection_update(range(num, 4001))
+
+        elif comparison == '>':
+            ratings_range[rating].intersection_update(range(1, num + 1))
 
 # Inspiration from day 10 for the area calculation
 if __name__ == '__main__':
@@ -48,7 +55,7 @@ if __name__ == '__main__':
         name, refuse = regex_tag_end.match(workflow).groups()
         logic = [(tag, comparison, int(num), out) for tag, comparison, num, out in regex_logic.findall(workflow)]
 
-        workflows_dict[name] = Workflow(name, refuse, logic)
+        workflows_dict[name] = Workflow(name, logic + [refuse])
 
     for part in parts_list:
         part_dict = {}
@@ -72,28 +79,32 @@ if __name__ == '__main__':
 
     print(f'Answer part 1: {calculate_sum_rating_scores(accepted_parts)}')
     
-
-    # Part 2: figure out the possible ranges for each rating -> work in reverse -> build a tree and walk back from each 'A' leaves (golden leaves)
+    # # Part 2: figure out the possible ranges for each rating -> work in reverse -> build a tree and walk back from each 'A' leaves (golden leaves)
 
     golden_leaves = []
+
     for workflow in workflows_dict.values():
-        for evaluation in workflow.logic:
+
+        for evaluation in workflow.logic[:-1]:
+
             if evaluation[-1] not in ['A', 'R']:
                 workflows_dict[evaluation[-1]].root = workflow.name
 
             elif evaluation[-1] == 'A':
                 golden_leaves.append([workflow.name, evaluation])
 
-        if workflow.refuse not in ['A', 'R']:
-            workflows_dict[workflow.refuse].root = workflow.name
+        if workflow.logic[-1] not in ['A', 'R']:
+            workflows_dict[workflow.logic[-1]].root = workflow.name
 
-        elif workflow.refuse == 'A':
-                golden_leaves.append([workflow.name, workflow.refuse])
+        elif workflow.logic[-1] == 'A':
+                golden_leaves.append([workflow.name, workflow.logic[-1]])
+
+    # print(golden_leaves)
             
-    ratings_range_all = []
+    # ratings_range_all = []
 
     for leaf in golden_leaves:
-        print(leaf, workflows_dict[leaf[0]].logic)
+        # print(leaf, workflows_dict[leaf[0]].logic, workflows_dict[workflows_dict[leaf[0]].root].logic)
         ratings_range = {
             'x': set(range(1, 4001)),
             'm': set(range(1, 4001)),
@@ -101,37 +112,48 @@ if __name__ == '__main__':
             's': set(range(1, 4001))
         }
         
-    #     current_name, evaluation = leaf
-    #     update_ranges(ratings_range, evaluation)
-    #     root = workflows_dict[current_name].root
+        current_name, evaluation = leaf
 
-    #     while root:
+        # If evaluation contains a rule, updates the range
+        if evaluation != 'A':
+            update_ranges(ratings_range, evaluation)
+
+        # Otherwise, update the opposite of the logic
+        else:
+            for eval in workflows_dict[current_name].logic[:-1]:
+                update_ranges(ratings_range, eval, False)
+
+
+        root = workflows_dict[current_name].root
+
+        while root:
             
-    #         evaluation = [evaluation for evaluation in workflows_dict[root].logic if current_name in evaluation]
+            evaluation = [evaluation for evaluation in workflows_dict[root].logic if current_name in evaluation]
 
-    #         if evaluation:
-    #             print(evaluation)
-    #             evaluation = evaluation[0]
-    #             update_ranges(ratings_range, evaluation)
+            if evaluation:
+                print(evaluation)
+                evaluation = evaluation[0]
+                
+                 # If evaluation contains a rule, updates the range
+                if evaluation != 'A':
+                    update_ranges(ratings_range, evaluation)
+
+                # Otherwise, update the opposite of the logic
+                else:
+                    for eval in workflows_dict[current_name].logic[:-1]:
+                        update_ranges(ratings_range, eval, False)
+
+            root, current_name = workflows_dict[root].root, root
+
             
-    #         elif workflows_dict[root].refuse == current_name:
-    #             print('refuse')
-    #         else:
-    #             print('WHAT', root, workflows_dict[root].logic, current_name)
 
-    #             break
+    # # combination = 1
+    # # for r in ratings_range:
+    # #     print(r, min(ratings_range[r]), max(ratings_range[r]))
+    # #     combination *=  max(ratings_range[r]) - min(ratings_range[r])
 
-    #         root, current_name = workflows_dict[root].root, root
+    # # print(combination)
+    # # print(combination < 167409079868000)
 
-            
-
-    # combination = 1
-    # for r in ratings_range:
-    #     print(r, min(ratings_range[r]), max(ratings_range[r]))
-    #     combination *=  max(ratings_range[r]) - min(ratings_range[r])
-
-    # print(combination)
-    # print(combination < 167409079868000)
-
-    # print(f'Answer part 2: {None}')
-    # print(f'Time :{timeit.default_timer() - start} s')
+    # # print(f'Answer part 2: {None}')
+    # # print(f'Time :{timeit.default_timer() - start} s')
